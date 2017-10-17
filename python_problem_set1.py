a9dafc120cd69a514a7c71c5ad204db532e20486
admin <admin@pfb11.cshl.edu>
2017-10-17 12:05:54 -0400

    added Python 1 problem set nano script

---
+++ python_problem_set1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys
+
+print ('Hello New York')
+name = 'Carrie Iwema'
+print (name)
+
+color = 'black'
+activity = 'rowing'
+animal = 'octopus'
+[ name, color, activity, animal ]
+favorites = [ name, color, activity, animal ]
+print (favorites)
+
+{ 'name' : 'Carrie Iwema' , 'color' : 'black' , 'activity' : 'rowing' , 'animal' : 'octopus' }
+
+
+fave = { 'name' : 'Carrie Iwema' , 'color' : 'black' , 'activity' : 'rowing' , 'animal' : 'octopus' }
+print (fave)
+
+a = sys.argv[1]
+b = sys.argv[2]
+c = sys.argv[3]
+d = sys.argv[4]
+
+print ('My name is', a)
+print ('My favorite color is', b)
+print ('My favorite new activity is', c)
+print ('My favorite animal is', d)
+ 
