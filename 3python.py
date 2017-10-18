fe36ad37e1e83f78980ac87c5cee990c885e82c7
admin <admin@pfb11.cshl.edu>
2017-10-18 17:01:51 -0400

    Python 3 problem set

--- 3python.py
+++ 3python.py
@@ -27,7 +27,55 @@ compG2c = compA2t.replace ('G', 'c')
 compC2g = compG2c.replace ('C', 'g')
 
 rev = compC2g [::-1]
-print ('The reverse complement of the DNA sequence is:\n',rev)
+REV = rev.upper()
+print ('The reverse complement of the DNA sequence is:\n',REV)
+
+#Q4 add/commit/push
+
+#Q5 write a script to find EcoR1 start site in above seq
+
+EcoR1 = 'GAATTC'
+ecor1_loc = (dna.find ('GAATTC') + 1)
+
+print ('The EcoR1 start position in this strand is',ecor1_loc)
+
+#Q6 location of cut site, along with first/last NTs for each fragment
+
+dna_loc = dna.split('GAATTC')
+
+frag1 = dna_loc[0]
+frag2 = dna_loc[1]
+
+start_frag1 = frag1[0]
+end_frag1 = frag1[-1]
+print ('The first NT of DNA fragment 1 is',start_frag1, 'and the last is', end_frag1)
+
+start_frag2 = frag2[0]
+end_frag2 = frag2[-1]
+print ('The first NT of DNA fragment 2 is',start_frag2, 'and the last is', end_frag2)
+
+#Q7 fragment extraction and position in entire sequence and length
+
+frag1_len = len(frag1)
+frag2_len = len(frag2)
+bothfrags = 'The first DNA fragment: {} is {} NTs long and the second fragment: {} is {} NTs long.'
+finalfrag = bothfrags.format(frag1, frag1_len, frag2, frag2_len)
+
+print (finalfrag)
+
+# for Q7 didn't id the seq position, but moving on to other things
+
+#Q8 create a list and add the fragments to it
+
+frag_list = [frag1 , frag2]
+print ('This is the list of both DNA fragments' , frag_list)
+
+#Q9 sorted list
+
+frag_list.sort()
+print ('This is a sorted list of the fragments:' , frag_list)
+
+#Q10 sorted by fragment length
 
 
 
