a08ce839e547ee73ee443012df4c987ce7c3180c
admin <admin@pfb11.cshl.edu>
2017-10-24 17:33:24 -0400

    updates beginning ch5

--- Python_Biologists.py
+++ Python_Biologists.py
@@ -180,3 +180,22 @@ output = open("genomic_exons.txt" , "w")
 output.write(codingseq)
 output.close()
 
+#Chapter 5
+
+def get_at_content(dna, sig_figs):
+   length = len(dna)
+   a_count = dna.upper().count('A')
+   t_count = dna.upper().count('T')
+   at_content = (a_count + t_count) / length
+   return round(at_content, sig_figs)
+
+test_dna = "ATGCATGCAACTGTAGC"
+print (get_at_content(test_dna, 1))
+print (get_at_content(test_dna, 2))
+print (get_at_content(test_dna, 3))
+
+#write functions to do one job at a time; e.g. calculate something, then print something NOT calculate and print something!
+
+
+
+
