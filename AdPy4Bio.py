11d6ca1ac6d4b33168400c3784eb0ef9fb1214f4
admin <admin@pfb11.cshl.edu>
2017-10-30 11:51:13 -0400

    through ch3

--- AdPy4Bio.py
+++ AdPy4Bio.py
@@ -97,3 +97,87 @@ def get_lca_list_rec(taxa):
 #----
 
 # Ch3 : Complex data structures
+
+dna = 'aattggaattggaattg'
+k = 4
+kmer2count = {}
+for start in range(len(dna) - k + 1): #iterate over each possible start position using a range
+    kmer = dna[start:start + k] #extract the kmer
+    current_count = kmer2count.get(kmer, 0) #look up current count for kmer from the dict
+    kmer2count[kmer] = current_count + 1 #update dict value to be current + 1
+print(kmer2count) #keys = kmers; values = counts
+
+
+dna = 'aattggaattggaattg'
+k = 4
+kmer2list = {} #rather than count, build up a list of start positions for each kmer
+for start in range(len(dna) - k + 1):
+    kmer = dna[start:start + k]
+    list_of_positions = kmer2list.get(kmer, [])
+    list_of_positions.append(start)
+    kmer2list[kmer] = list_of_positions
+print(kmer2list) #get a dict of lists
+
+#-----
+
+# Distance matrix
+
+# two parts: calculating similarity scores and creating dict to hold results
+
+gene_sets = {
+    'arsenic' : {1,2,3,4,5,6,8,12},
+    'cadmium' : {2,12,6,4},
+    'copper' : {7,6,10,4,8},
+    'mercury' : {3,2,4,5,1}
+}
+
+similarity_scores = {}
+for condition1, set1 in gene_sets.items():
+    single_scores = {
+        condition2 : len(set1.intersection(set2)) / len(set1.union(set2))
+        for condition2, set2 in gene_sets.items()
+        if condition1 != condition2
+        }
+    similarity_scores[condition1] = single_scores
+    print(condition1, single_scores)
+print(similarity_scores)
+
+#even shorter!
+
+similarity_scores = {
+    c1: {
+        c2 : len(s1.intersection(s2)) / len(s1.union(s2))
+        for c2,s2 in gene_sets.items()
+        if c1 != c2
+        }
+    for c1,s1 in gene_sets.items()
+}
+
+#-----
+
+# Testing for monophyly
+
+def are_closely_related(my_list, taxon1, taxon2, taxon3):
+    result = False
+    # does the current list match the condition?
+    if (contains(my_list, taxon1)
+    and contains(my_list, taxon2)
+    and not contains(my_list, taxon3)):
+        result = True
+    # do any sublists match the condition?
+    for sublist in my_list:
+        if isinstance(sublist, list):
+            if are_closely_related(sublist, taxon1, taxon2, taxon3):
+                result = True
+    return result
+
+assert are_closely_related(tree, 'raccoon', 'dog', 'bear') == False
+assert are_closely_related(tree, 'raccoon', 'bear', 'weasel') == True
+assert are_closely_related(tree, 'raccoon', 'bear', 'dog') == True
+
+# didn't run the above b/c wasn't sure of the values for my_list or the taxons
+
+#-----
+
+# Ch4 : Object oriented Python
+
